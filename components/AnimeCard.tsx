ef58df39138747f725438b2c186bda4b859fdfbc
Aman Varshney <amanvarshney212@gmail.com>
2023-12-24 14:55:17 +0530

    added animation with framer motion

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+// framer motion in server side rendered pages
+import { MotionDiv } from "./MotionDiv";
+
 export interface AnimeProp {
   id: string;
   name: string;
@@ -17,9 +20,29 @@ interface Prop {
   index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+function AnimeCard({ anime, index }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        duration: 0.5,
+        delay: index * 0.25,
+        ease: "easeInOut",
+      }}
+      viewport={{ amount: 0 }}
+      className="max-w-sm rounded relative w-full"
+    >
       <div className="relative w-full h-[37vh]">
         <Image
           src={`https://shikimori.one${anime.image.original}`}
@@ -64,7 +87,7 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -3,20 +3,23 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import AnimeCard, { AnimeProp } from "./AnimeCard";
+import AnimeCard from "./AnimeCard";
 import { fetchAnime } from "@/app/action";
 
 let page = 2;
+
+export type AnimeCard = JSX.Element;
 
 function LoadMore() {
   // it is a hook from react-intersection-observer
   // you cant use it in the server side
   // thats why we use useClient directive
   const { ref, inView } = useInView();
-  const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if (inView) {
+      // we can call same server side function from client side fetchAnime
       fetchAnime(page).then((res) => {
         setData([...data, ...res]);
         page++;
@@ -28,9 +31,7 @@ function LoadMore() {
     <>
       {/* this section is for all the subsequesnt pages from page 1 */}
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
